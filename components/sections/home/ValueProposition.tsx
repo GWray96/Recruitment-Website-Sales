27465d0640b703a1ca92b6a4f8c2e2c10a5472bb
G4rfy <g4rfy@example.com>
2025-04-10 21:18:43 +0100

    Fix ESLint issues by disabling unescaped entities rule and adding missing Link import

--- components/sections/home/ValueProposition.tsx
+++ components/sections/home/ValueProposition.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 import { BsSearch, BsBriefcase, BsGraphUp, BsShieldCheck } from 'react-icons/bs';
 
 const features = [
